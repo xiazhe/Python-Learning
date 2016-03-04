9d804d2db884c2b7acaa1d1a316076acf1499d28
Lucker Xia <lucker.xia@missionsky.com>
2016-03-04 18:07:05 +0800

    mini app

---
+++ mini.py
@@ -0,0 +1,11 @@
+# A Minimal Application
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(port=5555)
