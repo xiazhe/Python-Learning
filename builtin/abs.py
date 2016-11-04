0b6c6830e8a6e82ac426b2e9afa57bece872871a
Xia Zhe <xiazhe@outlook.com>
2016-11-04 12:48:00 +0800

    updated

--- builtin/abs.py
+++ builtin/abs.py
@@ -1,4 +1,6 @@
 # -*- coding: UTF-8 -*-
+# http://python.usyiyi.cn/python_278/library/functions.html
+# https://docs.python.org/2.7/library/functions.html
 
 # 取绝对值
 print abs(-1), abs(10), abs(1.1)
--- builtin/class_init.py
+++ builtin/class_init.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*
 # Filename: class_init.py
 # https://docs.python.org/2.7/library/functions.html
+# http://pyzh.readthedocs.io/en/latest/python-magic-methods-guide.html
 """
 __init__方法在类的一个对象被建立时，马上运行。
 这个方法可以用来对你的对象做一些你希望的初始化。注意，这个名称的开始和结尾都是双下划线。
