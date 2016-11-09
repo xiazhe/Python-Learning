2de75ce00cbc713c314ea5f12ea323331e529e7a
Xia Zhe <xiazhe@outlook.com>
2016-11-09 16:43:18 +0800

    make name pythonic

--- function.py
+++ function.py
@@ -7,73 +7,73 @@ class C(object):
     def __init__(self):
         pass
 
-    def instancefun(self):
+    def instanceFun(self):
         print('instance function', self.foo)
 
-    @staticmethod                      # 静态方法
-    def f(arg1, arg2):
-        pass
+    # @staticmethod                      # 静态方法
+    # def f(arg1, arg2):
+    #     pass
 
     @staticmethod
-    def f1():
+    def staticFunc():
         print('static method', C.foo)
 
-    @classmethod                       # 类方法
-    def f2(cls, arg1, arg2):
-        pass
+    # @classmethod                       # 类方法
+    # def f2(cls, arg1, arg2):
+    #     pass
 
     @classmethod
-    def f3(cls):
+    def classFunc(cls):
         print('class method', cls.__name__, cls.foo)
 
-    # TypeError: f4() takes no arguments (1 given)
-    def f4():
+    # TypeError: func() takes no arguments (1 given)
+    def func():
         print('Common Method')
 
 
 # C.f()
 
-# C().f1()
+# C().staticFunc()
 
 # 区别是什么， 应用场景
 # 类调用静态方法
-C.f4()
+C.func()
 
 # 实例调用静态方法
-C().f1()
+C().staticFunc()
 
 # 类调用类方法
-C.f3()
+C.classFunc()
 
 # 实例调用类方法
-C().f3()
+C().classFunc()
 
 # 类调用实例方法需要自己穿self 参数(C类本身)
-# C.instancefun()
-# TypeError: unbound method instancefun() must be called with C instance as first argument (got nothing instead)
+# C.instanceFun()
+# TypeError: unbound method instanceFun() must be called with C instance as first argument (got nothing instead)
 
-C.instancefun(C)
+C.instanceFun(C)
 
 # 实例化实例 c
 c = C()
-c.f3()
+c.classFunc()
 
-c.instancefun()
+c.instanceFun()
 
 # 类/实例属性改变， 调用静态方法， 类方法， 实例方法
 #
 # C.foo = 200
 c.foo = 200
 
-C.f1()
+C.staticFunc()
 
-C.f3()
+C.classFunc()
 
-c.f1()
+c.staticFunc()
 
-c.f3()
+c.classFunc()
 
-c.instancefun()
+c.instanceFun()
 
 
 print(c.foo)
@@ -104,9 +104,9 @@ print(c.foo)
 
 
 
-# c.f4() error
-C.f4()      # Python 3.5.2 允许运行  Common Method
-# python 2.7 中 TypeError: unbound method f4() must be called with C instance as first argument (got nothing instead)
+# c.func() error
+C.func()      # Python 3.5.2 允许运行  Common Method
+# python 2.7 中 TypeError: unbound method func() must be called with C instance as first argument (got nothing instead)
 
 
 
